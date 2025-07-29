fb2ffd0cc1681066ad98a7820a16829abea2f428
Wilson Freitas de Oliveira <wilsonfreitashgem@gmail.com>
2025-07-29 17:27:51 -0300

    modifiquei app_layout.tsx porque estava tentando acessar dois navegadores diferentes, ou seja: erro crítico

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,15 @@
-import { Stack } from 'expo-router';
+// app/_layout.tsx
 import { Drawer } from 'expo-router/drawer';
 import { CarrinhoProvider } from '../contexts/CarrinhoContext';
 
 export default function RootLayout() {
   return (
     <CarrinhoProvider>
-      <Stack
+      <Drawer
         screenOptions={{
           headerShown: true,
         }}
-      >
-        <Drawer.Screen name="index" options={{ title: 'Home' }} />
-        <Drawer.Screen name="produtos" options={{ title: 'Produtos' }} />
-        <Drawer.Screen name="produto-novo" options={{ title: 'Novo Produto' }} />
-        <Drawer.Screen name="carrinho" options={{ title: 'Carrinho' }} />
-        <Drawer.Screen name="dashboard" options={{ title: 'Dashboard' }} />
-        <Drawer.Screen name="vendas" options={{ title: 'Vendas' }} />
-      </Drawer>
+      />
     </CarrinhoProvider>
   );
 }
